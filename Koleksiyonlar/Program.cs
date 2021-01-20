bc81c660eefa4cae721b15b2fea6c20574fd10f7
mugetsu <mugetsu@MUGETSU>
2021-01-20 23:50:39 +0300

    20.01.2021 ders sonu

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] isimler = new string[] {"Engin","Murat","Murtaza","Rifki"};
+            //Console.WriteLine(isimler[0]);
+            //Console.WriteLine(isimler[1]);
+            //Console.WriteLine(isimler[2]);
+            //Console.WriteLine(isimler[3]);
+
+            List<string> isimler2 = new List<string> { "Engin", "Murat", "Murtaza", "Rifki" };
+            isimler2.Add("Mesud");
+            foreach (var isim in isimler2)
+            {
+                Console.WriteLine(isim);
+            }
+        }
+    }
+}
---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        public void Add(Product product)
+        {
+            Console.WriteLine(product.ProductName+"Eklendi");
+        }
+        public void Update(Product product)
+        {
+            Console.WriteLine(product.ProductName + "Eklendi");
+        }
+        public void Delete(Product product)
+        {
+            Console.WriteLine(product.ProductName + "Eklendi");
+        }
+
+    }
+}
