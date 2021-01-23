0621cb87f7dcb717238402f50917015a3c80fa43
mugetsu <mugetsu@MUGETSU>
2021-01-23 22:28:42 +0300

    23.01.2021

---
+++ Constructer/DictonaryList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+namespace Constructer
+{
+    class DictonaryList<K,V>
+    {
+        K[] keys1;
+        V[] values1;
+        
+
+        public DictonaryList()
+        {
+            keys1 = new K[0];
+            values1 = new V[0];        
+
+        }
+
+        public void Add(K key,V value)
+        {
+
+            K[] tempKeys = keys1;
+            V[] tempValues = values1;
+            keys1 = new K[tempKeys.Length+1];
+            values1 = new V[tempValues.Length+1];
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys1[i] = tempKeys[i];
+                values1[i] = tempValues[i];
+            }
+
+            keys1[keys1.Length - 1] = key;
+            values1[values1.Length - 1] = value;
+            Console.WriteLine(key);
+
+
+        }
+    }
+}
---
+++ Constructer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Constructer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DictonaryList<int,string> dictionary = new DictonaryList<int,string>();
+            dictionary.Add(3, "Hasan");
+
+        }
+    }
+}
